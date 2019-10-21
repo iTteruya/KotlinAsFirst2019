05cbd0453dd99f0c7051f3b45d0ea89d9c31edaf
itTeruya <timur.ivanov@outlook.com>
2019-10-22 01:39:08 +0300

    lesson4 fix, lesson5fix lesson6 in progress

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -243,7 +243,6 @@ fun plusMinus(expression: String): Int {
  * Пример: "Он пошёл в в школу" => результат 9 (индекс первого 'в')
  */
 fun firstDuplicateIndex(str: String): Int {
-    if (Regex("""[~!@#${'$'}%^&*()+-]""").containsMatchIn(str)) return -1
     val ex = str.toLowerCase()
     val w = str.split(" ")
     for (word in w) {
